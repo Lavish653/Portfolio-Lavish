58c40a8cefff9d9b5961fdb4a9156eb7e6df3df1
Lavish Singhal <94366414+Lavish653@users.noreply.github.com>
2023-04-12 21:39:12 +0530

    Add files via upload

---
+++ Portfolio_Lavish/JS/Script.js
@@ -0,0 +1,10 @@
+	function slideshow(){
+		 var x = document.getElementById('check-class');
+		 if(x.style.display == "none"){
+			x.style.display="block";
+			
+		 }else{
+			x.style.display="none";
+			
+		 }
+	}
